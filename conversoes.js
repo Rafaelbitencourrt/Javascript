f73976e94096ca1f3593097a8215f652b6263867
Rafael <rafaelbitencourrt@gmail.com>
2022-04-07 11:08:14 -0300

    update

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dado
+// booleanos
+
+///conversão implícita
+
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + Number(numeroString));
+
+/// conversão explicita
+
+//Number() Transforma em numero
+//String() Transforma em string
